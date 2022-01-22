cef37d36a7b16faf02cae508d62d81acd7d1cfd2
scmDev <seo.changmin.dev@gmail.com>
2022-01-22 22:07:34 +0900

    feat: add flash message using session and cookie
    > npm i connect-flash
    
    var flash = require('connect-flash');
    app.use(flash());
    
    // In the user.js
    router.get('/flash', function(req, res, next) {
      res.session.message = 'session msg';
      res.flash('message', 'flash msg');
      res.redirect('/users/flash/result');
    });
    
    router.get('/flash/result', function(req, res, next) {
      res.send(`${req.session.message} ${req.flash('message'});
    });
    
    // Guess
    'flash' is eventEmitter obj
    'res.flash('eve', 'text') wraps .on method
    'req.flash('eve') wraps .emit method

--- nodejs_textbook/chp6/learn-express/app.js
+++ nodejs_textbook/chp6/learn-express/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // Additional Package
 var session = require('express-session');
+var flash = require('connect-flash');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,6 +30,7 @@ app.use(session({
     secure: false,
   },
 }));
+app.use(flash());
 
 
 app.use('/', indexRouter);
--- nodejs_textbook/chp6/learn-express/routes/users.js
+++ nodejs_textbook/chp6/learn-express/routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/flash', function(req, res, next) {
+  req.session.message = "세션 메세지";
+  req.flash('message', 'flash 메세지');
+  res.redirect('/users/flash/result');
+});
+
+router.get('/flash/result', function(req, res, next) {
+  res.send(`${req.session.message} ${req.flash('message')}`);
+})
+
+
 module.exports = router;
