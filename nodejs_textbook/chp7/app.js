7d335c8a24703f25836f42355ec50db5d6377653
scmDev <seo.changmin.dev@gmail.com>
2022-02-01 14:53:52 +0900

    DB init & Set associate each tables (users, comments)

--- nodejs_textbook/chp7/app.js
+++ nodejs_textbook/chp7/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const nunjucks = require('nunjucks');
 
@@ -19,6 +20,33 @@ app.set('view engine', 'html');
 nunjucks.configure('views', {
   express: app,
   watch: true, // html 파일 변경시 템플릿 엔진 다시 렌더링.
+});
+
+sequelize.sync({ force:true })
+  .then(() => {
+    console.log('DB 연결 성공');
+  })
+  .catch((err) => { // Promise 객체에 꼭 .catch를 붙이는 것을 권장한다.
+    console.error(err);
+  });
+
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+
+app.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 
---
+++ nodejs_textbook/chp7/models/user.js
@@ -0,0 +1,43 @@
+const { Sequelize, Model } = require('sequelize');
+
+module.exports = class User extends Model {
+  static init(sequelize) {
+    return super.init({
+      name: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        unique: true,
+      },
+      age: {
+        type: Sequelize.INTEGER.UNSIGNED,
+        allowNull: false,
+      },
+      married: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      comment: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        allowNull: false,
+      },
+    }, {
+      sequelize,
+      timestamps: false,
+      underscored: false,
+      modelName: 'User',
+      tableName: 'users',
+      paranoid: false,
+      charset: 'utf8',
+      collate: 'utf8_general_ci',
+    });
+  }
+
+  static associate(db) {
+    db.User.hasMany(db.Comment, { foreignKey: 'commenter', sourceKey: 'id' });
+  }
+};
